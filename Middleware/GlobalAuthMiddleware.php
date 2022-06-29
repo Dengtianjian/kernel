2e55a45058ff5bf8b07a47c897ce9dafe7f5ad28
Tianjian <mail@isdtj.com>
2022-06-29 11:51:36 +0800

    增加可控制控制器内哪个请求方法才需要校验权限

--- Middleware/GlobalAuthMiddleware.php
+++ Middleware/GlobalAuthMiddleware.php
@@ -16,20 +16,19 @@ class GlobalAuthMiddleware
   private function verifyToken(Request $request, bool $strongCheck = true): void
   {
     $token = $request->headers("Authorization") ?: $request->query("Authorization") ?: $request->body("Authorization");
-    if ($strongCheck) {
-      if (empty($token) || !$token)
-        Response::error("Not_Auth");
+    if ($strongCheck && (empty($token) || !$token)) {
+      Response::error(401, "Auth:40101", "请登录后重试", [], "无TOKEN");
     }
     if (empty($token)) {
       return;
     }
     if (!preg_match("/^Bearer (.+)$/", $token, $token)) {
-      Response::error("Not_Auth");
+      Response::error(401, "Auth:40102", "请登录后重试", [], "头部的token参数错误");
     }
     $token = $token[1];
     if ($strongCheck) {
       if (empty($token) || !$token)
-        Response::error("Not_Auth");
+        Response::error(401, "Auth:40103", "请登录后重试", [], "头部Authorization值是空的");
     }
     if (empty($token)) {
       return;
@@ -39,14 +38,14 @@ class GlobalAuthMiddleware
     if ($token === null) {
       header("Authorization", "");
       if ($strongCheck) {
-        Response::error(401, "Auth:401001", "请登录后重试", [], "token无效");
+        Response::error(401, "Auth:40104", "请登录后重试", [], "token无效");
       }
     }
     $expirationDate = $token['createdAt'] + $token['expiration'];
     $diffDay = round((time() - $token['createdAt']) / 86400);
     $expirationDay = round($token['expiration'] / 86400);
     if (time() > $expirationDate || $diffDay > $expirationDay) {
-      Response::error(401, "Auth:401002", "登录已过期，请重新登录", [], "token失效");
+      Response::error(401, "Auth:40105", "登录已过期，请重新登录", [], "token失效");
     }
     header("Authorization:" . $token['token'] . "/" . $expirationDate, true);
     //* 如果token的有效期剩余20%，就自动刷新token
@@ -73,33 +72,55 @@ class GlobalAuthMiddleware
     }
 
     if (!class_exists($router['controller'])) {
-      throw new Error("Router controller(".$router['controller'].") not exists");
+      throw new Error("Router controller(" . $router['controller'] . ") not exists");
     }
 
     if (get_parent_class($router['controller']) === "kernel\Foundation\AuthController") {
-      if (method_exists($router['controller'], "Admin")) {
-        $adminMethodRM = new ReflectionMethod($router['controller'], "Admin");
-        if ($adminMethodRM->isStatic() && $router['controller']::Admin()) {
+      $needCheckAdmin = true;
+      $isAdminVerify = false;
+      if (is_array($router['controller']::$AdminMethods)) {
+        $methods = array_map(fn ($item) => strtolower($item), $router['controller']::$AdminMethods);
+        if (count($methods) && !in_array(strtolower($request->method), $methods)) {
+          $needCheckAdmin = false;
+        }
+      }
+      if ($needCheckAdmin) {
+        if (method_exists($router['controller'], "Admin")) {
+          $adminMethodRM = new ReflectionMethod($router['controller'], "Admin");
+          if ($adminMethodRM->isStatic() && $router['controller']::Admin()) {
+            $isAdminVerify = true;
+            $this->verifyToken($request);
+            $router['controller']::verifyAdmin();
+          }
+        } else if ($router['controller']::$Admin) {
           $isAdminVerify = true;
           $this->verifyToken($request);
           $router['controller']::verifyAdmin();
         }
-      } else if ($router['controller']::$Admin) {
-        $isAdminVerify = true;
-        $this->verifyToken($request);
-        $router['controller']::verifyAdmin();
       }
 
       if ($isAdminVerify === false) {
-        if (method_exists($router['controller'], "Auth")) {
-          $authMethodRM = new ReflectionMethod($router['controller'], "Auth");
-          if ($authMethodRM->isStatic() && $router['controller']::Auth()) {
+        $needCheckAuth = true;
+        if (is_array($router['controller']::$AuthMethods)) {
+          $methods = array_map(fn ($item) => strtolower($item), $router['controller']::$AuthMethods);
+          if (count($methods) && !in_array(strtolower($request->method), $methods)) {
+            $needCheckAuth = false;
+          }
+        }
+
+        if ($needCheckAuth) {
+          if (method_exists($router['controller'], "Auth")) {
+            $authMethodRM = new ReflectionMethod($router['controller'], "Auth");
+            if ($authMethodRM->isStatic() && $router['controller']::Auth()) {
+              $this->verifyToken($request);
+              $router['controller']::verifyAuth();
+            }
+          } else if ($router['controller']::$Auth) {
             $this->verifyToken($request);
             $router['controller']::verifyAuth();
+          } else {
+            $this->verifyToken($request, false);
           }
-        } else if ($router['controller']::$Auth) {
-          $this->verifyToken($request);
-          $router['controller']::verifyAuth();
         } else {
           $this->verifyToken($request, false);
         }
