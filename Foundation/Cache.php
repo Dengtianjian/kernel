532e9e561cb5db7aa6b742432cd5c21866083308
Tianjian <mail@isdtj.com>
2022-07-06 13:18:48 +0800

    缓存功能

---
+++ Foundation/Cache.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace kernel\Foundation;
+
+use kernel\Foundation\Data\Arr;
+
+class Cache
+{
+  static private $SaveBasePath = F_APP_ROOT . "/Data/Cache"; //* 缓存存储的基路径
+  static private $readedCaches = []; //* 已经读取的缓存
+  static private $readedCacheMetas = []; //* 已经读取的缓存元数据
+  static private $DaySeconeds = 60 * 60 * 24; //* 一天有多少秒
+  /**
+   * 读取缓存
+   *
+   * @param string $id 缓存ID，也是文件夹名称
+   * @return array|bool|null
+   */
+  static function read(string $id): array|bool|null
+  {
+    $targetPath = self::$SaveBasePath . "/$id.txt";
+    if (!file_exists($targetPath)) return false;
+    $cache = file_get_contents($targetPath);
+    $cache = unserialize($cache);
+
+    if ($cache['meta']['expiredAt'] < time()) {
+      return null;
+    }
+
+    self::$readedCaches[$id] = $cache['content'];
+    self::$readedCacheMetas[$id] = $cache['meta'];
+
+    return $cache['content'];
+  }
+  /**
+   * 读取缓存的 元数据
+   *
+   * @param string $id 缓存ID
+   * @return array|boolean|null
+   */
+  static function meta(string $id): array|bool|null
+  {
+    $result = self::read($id);
+    if ($result === false) {
+      return false;
+    }
+    return self::$readedCacheMetas[$id];
+  }
+  /**
+   * 写入缓存，该方法只会合并已有的缓存
+   *
+   * @param string $id 缓存ID
+   * @param array $content 缓存内容
+   * @param int $expiresIn 有效期（天）
+   * @return bool
+   */
+  static function write(string $id, array $content, int $expiresIn = 30): bool
+  {
+    if (!is_dir(self::$SaveBasePath)) {
+      mkdir(self::$SaveBasePath, 757, true);
+    }
+    $targetPath = self::$SaveBasePath . "/$id.txt";
+    if (!in_array($id, self::$readedCaches)) {
+      $cache = self::read($id);
+    } else {
+      $cache = [
+        "content" => [],
+        "meta" => [
+          "updatedAt" => time(),
+          "addedAt" => time(),
+          "expiredAt" => time() + self::$DaySeconeds * $expiresIn
+        ]
+      ];
+    }
+
+    $cache['meta']['updatedAt'] = time();
+    $cache['meta']['expiredAt'] = time() + self::$DaySeconeds * $expiresIn;
+    
+    $cache['content'] = Arr::merge($cache['content'], $content);
+
+    $result = file_put_contents($targetPath, serialize($cache));
+
+    return boolval($result);
+  }
+  /**
+   * 覆盖已有缓存
+   *
+   * @param string $id 缓存ID
+   * @param array $content 覆盖的内容
+   * @return bool
+   */
+  static function overwrite(string $id, array $content, int $expiresIn = 30): bool
+  {
+    if (!is_dir(self::$SaveBasePath)) {
+      mkdir(self::$SaveBasePath, 757, true);
+    }
+    $targetPath = self::$SaveBasePath . "/$id.txt";
+
+    $result = file_put_contents($targetPath, serialize([
+      "content" => $content,
+      "meta" => [
+        "updatedAt" => time(),
+        "addedAt" => time(),
+        "expiredAt" => time() + self::$DaySeconeds * $expiresIn
+      ]
+    ]));
+
+    self::$readedCaches[$id] = $content;
+
+    return boolval($result);
+  }
+  /**
+   * 清除已有缓存
+   *
+   * @param string $id 缓存ID
+   * @return bool
+   */
+  static function clear(string $id): bool
+  {
+    $targetPath = self::$SaveBasePath . "/$id.txt";
+    if (!file_exists($targetPath)) {
+      return self::overwrite($id, []);
+    }
+
+    $result = file_put_contents($targetPath, [
+      "content" => [],
+      "meta" => [
+        "updatedAt" => time(),
+        "addedAt" => time(),
+        "expiredAt" => time() + self::$DaySeconeds * 30
+      ]
+    ]);
+
+    return boolval($result);
+  }
+  /**
+   * 删除缓存
+   *
+   * @param string $id 缓存ID
+   * @return bool
+   */
+  static function remove(string $id): bool
+  {
+    $targetPath = self::$SaveBasePath . "/$id.txt";
+    if (!file_exists($targetPath)) return true;
+
+    unlink($targetPath);
+
+    return true;
+  }
+}
