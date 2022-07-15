8ad203c50c2347d9ca5de23d3222daa8a3ac5cd9
Tianjian <mail@isdtj.com>
2022-07-15 15:08:37 +0800

    0.1.12.20220715

--- Config.php
+++ Config.php
@@ -8,6 +8,6 @@ if (!defined("F_KERNEL")) {
 
 $Config = [
   "mode" => "development", //* production development
-  "version" => "0.1.11.20220710",
+  "version" => "0.1.12.20220715",
   "extensions" => false
 ];
