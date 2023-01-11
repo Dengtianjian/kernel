32b6caa8a904fa2fbf49a311c415b9190155721b
Tianjian <mail@isdtj.com>
2023-01-11 23:08:58 +0800

    组成路径是不移除前面的斜杠

--- Foundation/File.php
+++ Foundation/File.php
@@ -249,12 +249,12 @@ class File
   {
     return implode(DIRECTORY_SEPARATOR, array_map(function ($item) {
       $lastText = $item[strlen($item) - 1];
-      if ($lastText === "/" || $lastText === "\\") {
-        $item = substr($item, 0, strlen($item) - 1);
-      }
-      if ($item[0] === "/" || $item[0] === "\\") {
-        $item = substr($item, 1, strlen($item));
-      }
+      // if ($lastText === "/" || $lastText === "\\") {
+      //   $item = substr($item, 0, strlen($item) - 1);
+      // }
+      // if ($item[0] === "/" || $item[0] === "\\") {
+      //   $item = substr($item, 1, strlen($item));
+      // }
       return $item;
     }, array_filter($els, function ($item) {
       return !empty(trim($item));
