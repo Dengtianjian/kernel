b12e7fd57a1bab6516fd764a0a6f52e5423b1e81
Tianjian <mail@isdtj.com>
2023-02-11 17:04:57 +0800

    不要报全部错误

--- gstudio_kernel.inc.php
+++ gstudio_kernel.inc.php
@@ -8,7 +8,7 @@ use kernel\Middleware\GlobalTestMiddleware;
 use kernel\Middleware\GlobalWechatOfficialAccountMiddleware;
 use kernel\Platform\DiscuzX\Foundation\DiscuzXApp;
 
-error_reporting(E_ALL);
+// error_reporting(E_ALL);
 
 include_once DISCUZ_ROOT . "/source/plugin/gstudio_kernel/Platform/DiscuzX/Foundation/DiscuzXAutoload.php";
 
