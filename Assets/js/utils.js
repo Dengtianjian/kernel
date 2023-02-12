065e6f79d3bdc00113d5c11a94419e0a91f82f5c
Tianjian <mail@isdtj.com>
2023-02-13 01:00:32 +0800

    废弃部分静态文件

--- Assets/js/utils.js
+++
@@ -1,7 +0,0 @@
-function uploadImagePreview(file,selectors){
-  const fr=new FileReader();
-  fr.onload=function(e){
-    document.querySelector(selectors).setAttribute("src",fr.result);
-  }
-  fr.readAsDataURL(file);
-}
