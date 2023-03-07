46f68bb7eee87503de7951769ddea0196dcdfca6
Tianjian <mail@isdtj.com>
2023-03-07 23:05:45 +0800

    中间件基类优化

--- Foundation/Middleware.php
+++ Foundation/Middleware.php
@@ -3,10 +3,23 @@
 namespace kernel\Foundation;
 
 use Closure;
+use kernel\Foundation\Controller\Controller;
 use kernel\Foundation\HTTP\Request;
 
 class Middleware
 {
+  /**
+   * 请求实例
+   *
+   * @var Request
+   */
+  protected $request = null;
+  /**
+   * 控制器
+   *
+   * @var Controller|AuthController|Closure
+   */
+  protected $controller = null;
   /**
    * 中间件基类构建函数
    *
@@ -14,19 +27,9 @@ class Middleware
    * @param Controller|AuthController|null $Controller 控制器实例，如果控制器不是类是个闭包就会传入null
    * @return \kernel\Foundation\HTTP\Response
    */
-  public function __construct(Request $R, $Controller)
+  public function __construct(Request $request, Controller $controller)
   {
-  }
-  /**
-   * 中间件处理
-   *
-   * @param Closure $next 下一个中间件执行，会返回Response
-   * @param Request $R 请求实例
-   * @param Controller|AuthController|null $Controller 控制器实例，如果控制器不是类是个闭包就会传入null
-   * @return \kernel\Foundation\HTTP\Response
-   */
-  public function handle(\Closure $next, Request $request, $Controller = null)
-  {
-    return $next();
+    $this->request = $request;
+    $this->controller = $controller;
   }
 }
