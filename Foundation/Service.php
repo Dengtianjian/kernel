bf6b9eef045d5978b44cad93af5659e59a7d928b
Tianjian <mail@isdtj.com>
2023-05-14 03:19:04 +0800

    增加ReturnResult实例属性

--- Foundation/Service.php
+++ Foundation/Service.php
@@ -2,6 +2,8 @@
 
 namespace kernel\Foundation;
 
+use kernel\Foundation\ReturnResult\ReturnResult;
+
 if (!defined('F_KERNEL')) {
   exit('Access Denied');
 }
@@ -12,6 +14,17 @@ if (!defined("F_KERNEL")) {
 
 class Service extends BaseObject
 {
+  /**
+   * ReturnResult实例
+   *
+   * @var ReturnResult
+   */
+  protected $return = null;
+  public function __construct()
+  {
+    $this->return = new ReturnResult(true);
+  }
+
   private static $useParams = [];
   final static function setUseParams($params)
   {
