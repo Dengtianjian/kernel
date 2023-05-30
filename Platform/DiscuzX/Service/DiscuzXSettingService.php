70ed7a5bad3fdce6a9cbbd4cd70a7b7e7d2a5830
Tianjian <mail@isdtj.com>
2023-05-31 00:05:02 +0800

    优化dzx设置项服务类方法

--- Platform/DiscuzX/Service/DiscuzXSettingService.php
+++ Platform/DiscuzX/Service/DiscuzXSettingService.php
@@ -10,34 +10,40 @@ use kernel\Service\SettingService;
 
 class DiscuzXSettingService extends SettingService
 {
+  private static $SettingsTableName = null;
+  public static $Names = [];
+  public static $GroupNames = [];
+  public static $AdminNames = [];
   /**
    * 使用通用设置存储服务
    * 会存储可以获取的键名、注册settings为前缀路由
    *
-   * @param string $SettingsModel 设置表模型 xxx::class
    * @param array $names 都可以获取的键名
    * @param array $groupNames 不同用户组可以获取的键名，键是数组ID，值是键名数组 [ 1=>['appId','appName'],3=>['appName'] ]
    * @param array $adminNames 不同管理组可以获取的键名，键是数组ID，值是键名数组 [ 1=>['appId','appName'],3=>['appName'] ]
+   * @param string $SettingsTableaName 设置存储表名称，默认是appId+"_settings"
    * @return void
    */
-  static function init($SettingsModel, $names = [], $groupNames = [], $adminNames = [])
+  static function use(
+    $names = [],
+    $groupNames = [],
+    $adminNames = [],
+    $SettingsTableName = F_APP_ID . "_settings"
+  ) {
+    self::$SettingsTableName = $SettingsTableName;
+    self::$Names = $names;
+    self::$GroupNames = $groupNames;
+    self::$AdminNames = $adminNames;
+
+    Router::get("settings", GetSettingsController::class);
+    Router::patch("settings", SaveSettingsController::class);
+  }
+  static function init()
   {
-    self::setUseParams([
-      "names" => $names,
-      "groupNames" => $groupNames,
-      "adminNames" => $adminNames,
-      "SettingsModel" => $SettingsModel
-    ]);
-    self::registerRoute();
+    return (new DiscuzXSettingsModel(self::$SettingsTableName))->createTable();
   }
   public function __construct()
   {
-    $C = DiscuzXSettingService::getUseParams()['SettingsModel'];
-    $this->settingModel = new $C();
-  }
-  protected static function registerRoute()
-  {
-    Router::get("settings", GetSettingsController::class);
-    Router::patch("settings", SaveSettingsController::class);
+    $this->settingModel = new DiscuzXSettingsModel(self::$SettingsTableName);
   }
 }
