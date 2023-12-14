cd3ace9ffcff4b9c6bfe47197b0260f5ec034089
DengTianjian <mail@isdtj.com>
2023-12-14 21:54:44 +0800

    注册自动加载功能函数

---
+++ Platform/DiscuzX/Foundation/DiscuzXAutoloadRegister.php
@@ -0,0 +1,72 @@
+<?php
+
+// function loader($className, $SkipClassName = [], $SDKClassName = [])
+// {
+//   $className = str_replace("\\", "/", $className);
+//   if (strpos($className, "kernel") !== false && strpos($className, "gstudio_kernel") === false) {
+//     $className = str_replace("kernel", "gstudio_kernel", $className);
+//   }
+
+//   $SDKLoaded = false;
+
+//   foreach ($SDKClassName as $item) {
+//     $item = str_replace("\\", "/", $item);
+//     if (strpos($className, $item) !== false) {
+//       $className = F_APP_ID . "/SDK/{$className}";
+//       $SDKLoaded = true;
+//     }
+//   }
+//   if (!$SDKLoaded) {
+//     foreach ($SkipClassName as $item) {
+//       $item = str_replace("\\", "/", $item);
+//       if (strpos($className, $item) !== false) {
+//         return;
+//       }
+//     }
+//   }
+
+//   $filePath = DISCUZ_ROOT . "/source/plugin/$className.php";
+//   if (\file_exists($filePath)) {
+//     include_once($filePath);
+//   } else {
+//     if (strpos($filePath, "gstudio") !== false && defined("F_APP_MODE") && F_APP_MODE === "development") {
+//       debug($filePath);
+//     }
+//   }
+// }
+
+return function ($SkipClassName = [], $SDKClassName = []) {
+  spl_autoload_register(function ($className) use ($SkipClassName, $SDKClassName) {
+    $className = str_replace("\\", "/", $className);
+    if (strpos($className, "kernel") !== false && strpos($className, "gstudio_kernel") === false) {
+      $className = str_replace("kernel", "gstudio_kernel", $className);
+    }
+
+    $SDKLoaded = false;
+
+    foreach ($SDKClassName as $item) {
+      $item = str_replace("\\", "/", $item);
+      if (strpos($className, $item) !== false) {
+        $className = F_APP_ID . "/SDK/{$className}";
+        $SDKLoaded = true;
+      }
+    }
+    if (!$SDKLoaded) {
+      foreach ($SkipClassName as $item) {
+        $item = str_replace("\\", "/", $item);
+        if (strpos($className, $item) !== false) {
+          return;
+        }
+      }
+    }
+
+    $filePath = DISCUZ_ROOT . "/source/plugin/$className.php";
+    if (\file_exists($filePath)) {
+      include_once($filePath);
+    } else {
+      if (strpos($filePath, "gstudio") !== false && defined("F_APP_MODE") && F_APP_MODE === "development") {
+        debug($filePath);
+      }
+    }
+  }, true, true);
+};
