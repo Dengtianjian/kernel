9832559a28d3b13e618b7108c815f6708fac47c5
DengTianjian <mail@isdtj.com>
2023-12-27 00:12:18 +0800

    上传文件接口

--- Controller/Main/Files/UploadFilesController.php
+++ Controller/Main/Files/UploadFilesController.php
@@ -2,25 +2,20 @@
 
 namespace kernel\Controller\Main\Files;
 
-use kernel\Foundation\Store;
-use kernel\Service\FileStoreService;
-use superApp\Foundation\SuperAppAuthController;
+use kernel\Foundation\Controller\Controller;
+use kernel\Foundation\File\FileHelper;
+use kernel\Service\FileStorageService;
 
-class UploadFilesController extends SuperAppAuthController
+class UploadFilesController extends Controller
 {
-  public $query = [
-    "auth" => "boolean"
-  ];
   public function data()
   {
-    if (!Store::getApp("userId") || !Store::getApp("user")['admin']) {
-      return $this->response->error(403, 403, "抱歉，您没有权限上传附件");
-    }
-
-    $File = $_FILES['file'];
     if (count($_FILES) === 0 || !$_FILES['file']) {
       return $this->response->error(400, "UploadFile:400001", "请上传文件", $_FILES);
     }
-    return FileStoreService::save($_FILES['file'], "files", null, $this->query->has("auth") ? $this->query->get("auth") : true);
+    $UploadedResult = FileStorageService::upload($_FILES['file'], FileHelper::combinedFilePath(F_APP_DATA, "Files"));
+    if ($UploadedResult->error) return $UploadedResult;
+
+    return $UploadedResult->getData("fileKey");
   }
 }
