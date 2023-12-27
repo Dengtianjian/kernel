4d1ce1ec6a97d1038272513637ceaa29bdc2a13d
DengTianjian <mail@isdtj.com>
2023-12-27 15:57:30 +0800

    覆写初始化方法

--- Service/FileStorageService.php
+++ Service/FileStorageService.php
@@ -3,6 +3,7 @@
 namespace kernel\Service;
 
 use kernel\Controller\Main\Files as FilesNamespace;
+use kernel\Foundation\Database\PDO\DB;
 use kernel\Foundation\File\FileHelper;
 use kernel\Foundation\File\FileStorage;
 use kernel\Foundation\HTTP\URL;
@@ -24,6 +25,32 @@ class FileStorageService extends Service
     Router::get("files/{fileId:.+?}", FilesNamespace\AccessFileController::class);
     Router::get("files/{fileId:.+?}/download", FilesNamespace\DownloadFileController::class);
     Router::get("files/{fileId:.+?}/info", FilesNamespace\GetFileController::class);
+  }
+  static function init()
+  {
+    $SQL = <<<EOT
+DROP TABLE IF EXISTS `pre_gstudio_super_app_files`;
+CREATE TABLE `pre_gstudio_super_app_files`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '附件数字ID',
+  `key` text NOT NULL COMMENT '文件名',
+  `remote` tinyint(4) NOT NULL DEFAULT 0 COMMENT '远程附件',
+  `belongsId` varchar(34) NULL DEFAULT NULL COMMENT '所属ID',
+  `belongsType` varchar(32) NULL DEFAULT NULL COMMENT '所属ID类型',
+  `authId` varchar(32) NOT NULL DEFAULT '0' COMMENT '授权ID',
+  `sourceFileName` varchar(255) NOT NULL COMMENT '原本的文件名称',
+  `fileName` varchar(255) NOT NULL COMMENT '保存后的文件名称',
+  `fileSize` double NOT NULL COMMENT '文件尺寸',
+  `filePath` text NOT NULL COMMENT '保存的文件路径',
+  `width` double NULL DEFAULT 0 COMMENT '宽度（媒体文件才有该值）',
+  `height` double NULL DEFAULT 0 COMMENT '高度（媒体文件才有该值）',
+  `extension` varchar(30) NOT NULL COMMENT '文件扩展名',
+  `createdAt` varchar(12) NOT NULL COMMENT '创建时间',
+  `updatedAt` varchar(12) NOT NULL COMMENT '最后更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `userId`(`authId`) USING BTREE COMMENT '用户ID'
+) COMMENT = '文件';
+EOT;
+    DB::query($SQL);
   }
   /**
    * 上传文件
@@ -54,7 +81,7 @@ class FileStorageService extends Service
    * @param string $HTTPMethod 请求方式
    * @return ReturnResult<boolean> 是否已删除，true=删除完成，false=删除失败
    */
-  static function deleteFile($FileKey, $Signature = null, $SignatureKey = null,  $RawURLParams = [], $RawHeaders = [], $AuthId = null, $HTTPMethod = "get")
+  static function deleteFile($FileKey, $Signature = null, $SignatureKey = null, $RawURLParams = [], $RawHeaders = [], $AuthId = null, $HTTPMethod = "get")
   {
     $R = new ReturnResult(true);
     if ($Signature) {
@@ -62,7 +89,8 @@ class FileStorageService extends Service
         $RawURLParams['signature'] = $Signature;
       }
       $verifyResult = FileStorage::verifyAccessAuth($SignatureKey, $FileKey, $RawURLParams, $RawHeaders, $AuthId, $HTTPMethod);
-      if ($verifyResult !== false) return $R->error(403, 403, "签名错误", $verifyResult);
+      if ($verifyResult !== false)
+        return $R->error(403, 403, "签名错误", $verifyResult);
     }
 
     $FilePath = FileHelper::optimizedPath(FileHelper::combinedFilePath(F_APP_STORAGE, $FileKey));
@@ -92,7 +120,8 @@ class FileStorageService extends Service
         $RawURLParams['signature'] = $Signature;
       }
       $verifyResult = FileStorage::verifyAccessAuth($SignatureKey, $FileKey, $RawURLParams, $RawHeaders, $AuthId, $HTTPMethod);
-      if ($verifyResult !== true) return $R->error(403, 403, "签名错误", $verifyResult);
+      if ($verifyResult !== true)
+        return $R->error(403, 403, "签名错误", $verifyResult);
     }
 
     $FilePath = FileHelper::optimizedPath(FileHelper::combinedFilePath(F_APP_STORAGE, $FileKey));
