2ab9f504c8cf00460cf34d4180afddcc19153d32
DengTianjian <mail@isdtj.com>
2023-12-29 13:10:34 +0800

    文件表模型完善

--- Model/FilesModel.php
+++ Model/FilesModel.php
@@ -3,10 +3,12 @@
 namespace kernel\Model;
 
 use kernel\Foundation\Database\PDO\Model;
+use kernel\Traits\Model\FilesModelTrait;
 
 class FilesModel extends Model
 {
-  public $tableName = "files";
+  use FilesModelTrait;
+
   public function __construct()
   {
     $this->tableStructureSQL = <<<SQL
@@ -32,76 +34,7 @@ CREATE TABLE `files`  (
   INDEX `userId`(`authId`) USING BTREE COMMENT '用户ID'
 ) COMMENT = '文件';
 SQL;
-  }
-  function add($FileKey, $SourceFileName, $SaveFileName, $FilePath, $FileSize, $Extension, $OwnerId = null, $ACL = 'private', $Remote = false, $BelongsId = null, $BelongsType = null, $Width = 0, $Height = 0)
-  {
-    return $this->insert([
-      "key" => $FileKey,
-      "sourceFileName" => $SourceFileName,
-      "fileName" => $SaveFileName,
-      "filePath" => $FilePath,
-      "fileSize" => $FileSize,
-      "extension" => $Extension,
-      "remote" => $Remote,
-      "belongsId" => $BelongsId,
-      "belongsType" => $BelongsType,
-      "ownerId" => $OwnerId,
-      "width" => $Width,
-      "height" => $Height,
-      "acl" => $ACL
-    ]);
-  }
-  function save($Data, $FileKey = null, $Id = null)
-  {
-    return $this->filterNullWhere([
-      "id" => $Id,
-      "key" => $FileKey
-    ])->update($Data);
-  }
-  function updateBelongs($BelongsId = null, $BelongsType = null, $FileKey = null, $Id = null)
-  {
-    return $this->save([
-      "belongsId" => $BelongsId,
-      "belongsType" => $BelongsType,
-    ], $FileKey, $Id);
-  }
-  function item($FileKey = null, $BelongsId = null, $BelongsType, $OwnerId = null, $Id = null)
-  {
-    return $this->filterNullWhere([
-      "id" => $Id,
-      "key" => $FileKey,
-      "ownerId" => $OwnerId,
-      "belongsId" => $BelongsId,
-      "belongsType" => $BelongsType,
-    ])->getOne();
-  }
-  private $ListTotal = 0;
-  function listTotal()
-  {
-    return $this->ListTotal;
-  }
-  function list($Page = 1, $PerPage = 10, $FileKey = null, $BelongsId = null, $BelongsType, $OwnerId = null, $Id = null)
-  {
-    $this->ListTotal = $this->filterNullWhere([
-      "id" => $Id,
-      "key" => $FileKey,
-      "ownerId" => $OwnerId,
-      "belongsId" => $BelongsId,
-      "belongsType" => $BelongsType,
-    ])->reset(false)->count();
 
-    $this->page($Page, $PerPage);
-
-    return $this->getAll();
-  }
-  function remove($directly = false, $FileKey = null, $BelongsId = null, $BelongsType, $OwnerId = null, $Id = null)
-  {
-    return $this->filterNullWhere([
-      "id" => $Id,
-      "key" => $FileKey,
-      "ownerId" => $OwnerId,
-      "belongsId" => $BelongsId,
-      "belongsType" => $BelongsType,
-    ])->delete($directly);
+    parent::__construct("files");
   }
 }
