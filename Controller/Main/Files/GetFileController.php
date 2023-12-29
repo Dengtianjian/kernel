5a15aadb42f448ba811d13ec85c047cf6af2d576
DengTianjian <mail@isdtj.com>
2023-12-29 14:54:41 +0800

    增加序列化输出

--- Controller/Main/Files/GetFileController.php
+++ Controller/Main/Files/GetFileController.php
@@ -7,6 +7,16 @@ use kernel\Service\File\FileService;
 
 class GetFileController extends AuthController
 {
+  public $serializes = [
+    "fileKey" => "string",
+    "path" => "string",
+    "fileName"=>"string",
+    "extension" => "string",
+    "size" => "int",
+    "relativePath" => "string",
+    "width" => "double",
+    "height" => "double"
+  ];
   public function data($FileKey)
   {
     return FileService::getFileInfo($FileKey);
