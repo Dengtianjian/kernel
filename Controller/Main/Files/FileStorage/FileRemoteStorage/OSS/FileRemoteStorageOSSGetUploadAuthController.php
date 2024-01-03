c84cae433b02880bf3cbbf51023c95676d076070
DengTianjian <mail@isdtj.com>
2024-01-03 14:07:31 +0800

    OSS获取上传授权控制器

---
+++ Controller/Main/Files/FileStorage/FileRemoteStorage/OSS/FileRemoteStorageOSSGetUploadAuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace kernel\Controller\Main\Files\FileStorage\FileRemoteStorage\OSS;
+
+use kernel\Foundation\File\FileHelper;
+use kernel\Foundation\File\FileRemoteStorage;
+use kernel\Foundation\File\Files;
+use kernel\Foundation\File\FileStorage;
+use kernel\Foundation\Validate\ValidateRules;
+use kernel\Platform\DiscuzX\Foundation\DiscuzXController;
+use kernel\Platform\DiscuzX\Model\DiscuzXFilesModel;
+use kernel\Service\OSS\ObjectStorageService;
+use kernel\Service\OSS\OSSService;
+
+class FileRemoteStorageOSSGetUploadAuthController extends DiscuzXController
+{
+  public $body = [
+    "filePath" => "string",
+    "sourceFileName" => "string",
+    "scope" => "array",
+    "size" => "int"
+  ];
+  public function __construct($R)
+  {
+    $this->bodyValidator = [
+      "sourceFileName" => (new ValidateRules())->type("string", "请传入正确的原文件名称")->required("请传入原文件名称")->minLength(1, "请传入正确的原文件名称")->custom(function ($value) {
+        $PathInfo = pathinfo($value);
+        if (!$PathInfo['basename']) {
+          return $this->response->error(400, 400, "请传入正确的原文件名称");
+        }
+        if (!$PathInfo['extension']) {
+          return $this->response->error(400, 400, "请传入正确的原文件名称");
+        }
+
+        return $this->response->success(true);
+      })
+    ];
+
+    parent::__construct($R);
+  }
+  public function data()
+  {
+    $Body = $this->body->some();
+
+    $FilePathInfo = pathinfo($Body['sourceFileName']);
+
+    $ObjectFileName = uniqid() . "." . $FilePathInfo['extension'];
+    $FileKey = FileRemoteStorage::combinedFileKey($Body['filePath'], $ObjectFileName);
+
+    $Auth = OSSService::getAccessAuth($FileKey, 600, [], [], "put");
+    if ($Auth->error) return $Auth;
+    $FileName = $FilePathInfo['basename'];
+
+    DiscuzXFilesModel::singleton()->add($FileKey, $Body['sourceFileName'], $ObjectFileName, $Body['filePath'], $Body['size'], $FilePathInfo['extension'], null, FileStorage::PRIVATE, true);
+
+    return [
+      "fileKey" => $FileKey,
+      "sourceFileName" => $Body['sourceFileName'],
+      "filePath" => $Body['filePath'],
+      "fileName" =>  $FileName,
+      "size" => $Body['size'],
+      "extension" => $FilePathInfo['extension'],
+      "auth" => $Auth->getData()
+    ];
+  }
+}
