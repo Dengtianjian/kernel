021cae097c5ea5e69cac7083b5455efce26f9e7a
DengTianjian <mail@isdtj.com>
2024-01-06 09:23:50 +0800

    优化完善获取远程授权控制器

--- Controller/Main/Files/GetUploadRemoteAuthController.php
+++ Controller/Main/Files/GetUploadRemoteAuthController.php
@@ -4,8 +4,8 @@ namespace kernel\Controller\Main\Files;
 
 use kernel\Foundation\Controller\AuthController;
 use kernel\Foundation\File\Files;
+use kernel\Foundation\Validate\ValidateRules;
 use kernel\Service\File\FileRemoteStorageService;
-use kernel\Service\File\FileStorageService;
 
 /**
  * 获取上传到远程存储授权信息  
@@ -15,28 +15,49 @@ use kernel\Service\File\FileStorageService;
 class GetUploadRemoteAuthController extends AuthController
 {
   public $body = [
-    "fileName" => "string",
     "filePath" => "string",
-    "rename" => "boolean"
+    "sourceFileName" => "string",
+    "scope" => "array",
+    "size" => "int"
   ];
+  public function __construct($R)
+  {
+    $this->bodyValidator = [
+      "sourceFileName" => (new ValidateRules())->type("string", "请传入正确的原文件名称")->required("请传入原文件名称")->minLength(1, "请传入正确的原文件名称")->custom(function ($value) {
+        $PathInfo = pathinfo($value);
+        if (!$PathInfo['basename']) {
+          return $this->response->error(400, 400, "请传入正确的原文件名称");
+        }
+        if (!$PathInfo['extension']) {
+          return $this->response->error(400, 400, "请传入正确的原文件名称");
+        }
+
+        return $this->response->success(true);
+      })
+    ];
+
+    parent::__construct($R);
+  }
+
   public function data()
   {
     $Body = $this->body->some();
 
-    $fileName = $sourceFileName = $Body['fileName'];
-    if (!$this->body->has("rename") || $Body['rename']) {
-      $FileInfo = pathinfo($sourceFileName);
-      $fileName = uniqid() . "." . $FileInfo['extension'];
-    }
-    $FileKey = Files::combinedFileKey($Body['filePath'], $fileName);
+    $FilePathInfo = pathinfo($Body['sourceFileName']);
+
+    $ObjectFileName = uniqid() . "." . $FilePathInfo['extension'];
+    $FileKey = Files::combinedFileKey($Body['filePath'], $ObjectFileName);
+
     $Auth = FileRemoteStorageService::getFileAuth($FileKey, 600, [], [], "post");
     if ($Auth->error) return $Auth;
 
     return [
-      "sourceFileName" => $sourceFileName,
-      "fileName" => $fileName,
-      "filePath" => $Body['filePath'],
       "fileKey" => $FileKey,
+      "sourceFileName" => $Body['sourceFileName'],
+      "filePath" => $Body['filePath'],
+      "fileName" => pathinfo($FileKey, PATHINFO_BASENAME),
+      "size" => $Body['size'],
+      "extension" => $FilePathInfo['extension'],
       "auth" => $Auth->getData()
     ];
   }
---
+++ Platform/DiscuzX/Service/DiscuzXSettingsService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace kernel\Platform\DiscuzX\Service;
+
+use kernel\Foundation\Router;
+use kernel\Platform\DiscuzX\Controller\Settings\GetSettingsController;
+use kernel\Platform\DiscuzX\Controller\Settings\SaveSettingsController;
+use kernel\Platform\DiscuzX\Model\DiscuzXSettingsModel;
+use kernel\Service\SettingService;
+use kernel\Service\SettingsService;
+
+class DiscuzXSettingsService extends SettingsService
+{
+  protected static $SettingsTableName = null;
+  public static $Names = [];
+  public static $GroupNames = [];
+  public static $AdminNames = [];
+  /**
+   * 使用通用设置存储服务
+   * 会存储可以获取的键名、注册settings为前缀路由
+   *
+   * @param array $names 都可以获取的键名
+   * @param array $groupNames 不同用户组可以获取的键名，键是数组ID，值是键名数组 [ 1=>['appId','appName'],3=>['appName'] ]
+   * @param array $adminNames 不同管理组可以获取的键名，键是数组ID，值是键名数组 [ 1=>['appId','appName'],3=>['appName'] ]
+   * @param string $SettingsTableaName 设置存储表名称，默认是appId+"_settings"
+   * @return void
+   */
+  static function useService(
+    $names = [],
+    $groupNames = [],
+    $adminNames = [],
+    $SettingsTableName = null
+  ) {
+    self::$SettingsTableName = $SettingsTableName ?: F_APP_ID . "_settings";
+    self::$Names = $names;
+    self::$GroupNames = $groupNames;
+    self::$AdminNames = $adminNames;
+
+    Router::get("settings", GetSettingsController::class);
+    Router::patch("settings", SaveSettingsController::class);
+  }
+  static function init()
+  {
+    self::$SettingsTableName = self::$SettingsTableName ?: F_APP_ID . "_settings";
+    return (new DiscuzXSettingsModel(self::$SettingsTableName))->createTable();
+  }
+  public function __construct()
+  {
+    self::$settingsModel = new DiscuzXSettingsModel(self::$SettingsTableName);
+  }
+}
