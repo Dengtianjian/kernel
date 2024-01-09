d4c1aaf2701485dda491e5ea4e3623f053607920
DengTianjian <mail@isdtj.com>
2024-01-09 14:40:03 +0800

    删除旧的dzx平台控制器

--- Platform/DiscuzX/Controller/Files/DiscuzXAccessFileController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace kernel\Platform\DiscuzX\Controller\Files;
-
-use kernel\Controller\Main\Files\AccessFileController;
-
-class DiscuzXAccessFileController extends AccessFileController
-{
-}
