5dc335b48a225b9c32a757b1abab8eaf84d0a60f
DengTianjian <mail@isdtj.com>
2024-01-10 11:37:25 +0800

    增加能力基类和数据对象基类

---
+++ Foundation/Object/AbilityBaseObject.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace kernel\Foundation\Object;
+
+use kernel\Foundation\ReturnResult\ReturnResult;
+
+/**
+ * 能力基础对象  
+ * 适用于作为提供功能、能力类的基类
+ */
+class AbilityBaseObject
+{
+  /**
+   * 是否有错误
+   *
+   * @var boolean
+   */
+  protected $error = FALSE;
+  /**
+   * 错误码
+   *
+   * @var int
+   */
+  protected $errorCode = NULL;
+  /**
+   * 错误信息
+   *
+   * @var string
+   */
+  protected $errorMessage = NULL;
+  /**
+   * HTTP响应状态码
+   *
+   * @var int
+   */
+  protected $errorStatusCode = NULL;
+  /**
+   * 错误详情
+   *
+   * @var mixed
+   */
+  protected $errorDetails = NULL;
+  /**
+   * 错误数据
+   *
+   * @var mixed
+   */
+  protected $errorData = NULL;
+  /**
+   * 错误响应
+   *
+   * @param integer $statusCode HTTP状态码
+   * @param integer|string $code 响应码
+   * @param string $message 响应信息
+   * @param boolean $return 直接返回ReturnResult
+   * @param mixed $data 主体数据
+   * @param mixed $details 错误详情
+   * @return this
+   */
+  final protected function setError(
+    $statusCode = 500,
+    $code = 500,
+    $message = "error",
+    $return = FALSE,
+    $details = [],
+    $data = []
+  ) {
+    $this->error = TRUE;
+    $this->errorStatusCode = $statusCode;
+    $this->errorCode = $code;
+    $this->errorMessage = $message;
+    $this->errorDetails = $details;
+    $this->errorData = $data;
+
+    if ($return) return $this->return();
+
+    return $this;
+  }
+  /**
+   * 设置错误并且该函数会返回指定的值  
+   * 与setError不同的是该方法适用于直接return false
+   * public function(){
+   * \# code
+   *  return $this-interrupt(500,500,"服务错误"); // false
+   * }
+   *
+   * @param integer $statusCode HTTP状态码
+   * @param integer|string $code 响应码
+   * @param string $message 响应信息
+   * @param mixed $data 主体数据
+   * @param mixed $details 错误详情
+   * @return false
+   */
+  final protected function break(
+    $statusCode = 500,
+    $code = 500,
+    $message = "error",
+    $details = [],
+    $data = []
+  ) {
+    $this->setError($statusCode, $code, $message, FALSE, $details, $data);
+
+    return FALSE;
+  }
+  /**
+   * 获取错误信息
+   *
+   * @return string
+   */
+  final public function getErrorMessage()
+  {
+    return $this->errorMessage;
+  }
+  /**
+   * 获取错误码
+   *
+   * @return int|string
+   */
+  final public function getErrorCode()
+  {
+    return $this->errorCode;
+  }
+  /**
+   * 获取错误状态码
+   *
+   * @return int
+   */
+  final public function getStatusCode()
+  {
+    return $this->errorStatusCode;
+  }
+  /**
+   * 获取错误详情
+   *
+   * @return mixed
+   */
+  final public function getErrorDetails()
+  {
+    return $this->errorDetails;
+  }
+  /**
+   * 获取错误数据
+   *
+   * @return mixed
+   */
+  final public function getErrorData()
+  {
+    return $this->errorData;
+  }
+  /**
+   * 返回错误
+   *
+   * @return ReturnResult
+   */
+  final public function return()
+  {
+    return (new ReturnResult(FALSE))->error($this->errorStatusCode, $this->errorCode, $this->errorMessage, $this->errorDetails, $this->errorData);
+  }
+}
---
+++ Foundation/Object/DataObject.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace kernel\Foundation\Object;
+
+use kernel\Foundation\Exception\Exception;
+use stdClass;
+
+/**
+ * 文件信息
+ * @property string $key 文件键
+ * @property string $name 文件名称
+ */
+class DataObject extends stdClass
+{
+  public function __construct($Data)
+  {
+    $Vars = get_class_vars(get_class($this));
+    foreach (array_keys($Vars) as $key) {
+      $this->$key = $Data[$key];
+    }
+  }
+  function __get($name)
+  {
+    return $this->$name;
+  }
+  function __set($k, $v)
+  {
+    throw new Exception("数据对象只允许实例化时设置数据");
+  }
+  /**
+   * 将属性输出为数组格式
+   *
+   * @return array
+   */
+  final function toArray()
+  {
+    $Vars = get_class_vars(get_class($this));
+    $Data = [];
+    foreach (array_keys($Vars) as $key) {
+      $Data[$key] = $this->$key;
+    }
+    return $Data;
+  }
+  final function __toString()
+  {
+    $data = $this->toArray();
+
+    return json_encode($data);
+  }
+}
