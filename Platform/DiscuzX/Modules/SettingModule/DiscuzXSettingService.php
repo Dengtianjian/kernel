6b1291b716d5c8db061fe127c7d856925efe3fe5
DengTianjian <mail@isdtj.com>
2024-01-12 21:59:09 +0800

    增加控制是否注册路由

--- Platform/DiscuzX/Modules/SettingModule/DiscuzXSettingService.php
+++ Platform/DiscuzX/Modules/SettingModule/DiscuzXSettingService.php
@@ -13,21 +13,25 @@ class DiscuzXSettingService extends SettingService
    * 会注册获取设置项、保存设置项的路由
    *
    * @param DiscuzXSettingModuleBase $settingBase 设置功能模块实例
+   * @param boolean $RegisterRouter 是否注册路由
    * @return void
    */
   static function useService(
-    $settingBase = NULL
+    $settingBase = NULL,
+    $RegisterRouter = TRUE
   ) {
     if (is_null($settingBase)) {
       $settingBase = new DiscuzXSettingModuleBase(new DiscuzXSettingsModel());
     }
 
-    Router::get("settings", SettingsNamespace\DiscuzXGetSettingsController::class, [], [
-      $settingBase
-    ]);
-    Router::patch("settings", SettingsNamespace\DiscuzXSaveSettingsController::class, [], [
-      $settingBase
-    ]);
+    if ($RegisterRouter) {
+      Router::get("settings", SettingsNamespace\DiscuzXGetSettingsController::class, [], [
+        $settingBase
+      ]);
+      Router::patch("settings", SettingsNamespace\DiscuzXSaveSettingsController::class, [], [
+        $settingBase
+      ]);
+    }
 
     parent::useService($settingBase);
   }
