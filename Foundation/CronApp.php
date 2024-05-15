914cb044f7f4be43e297082c3f385a521eb967a9
DengTianjian <mail@isdtj.com>
2024-05-15 22:05:15 +0800

    增加app启动、结束钩子

--- Foundation/CronApp.php
+++ Foundation/CronApp.php
@@ -34,13 +34,24 @@ class CronApp extends App
       }
     }
   }
-  protected function executeController($Controller)
+  public function executeController($callTarget, $callParams, &$Controller)
   {
     $Ins = new $Controller($this->request());
     $Ins->data();
   }
   function run()
   {
+    //* 调用生命周期“启动”钩子
+    if ($this->LifeCycle['bootUp']) {
+      foreach ($this->LifeCycle['bootUp'] as $item) {
+        if (is_callable($item)) {
+          $item($this->request);
+        } else {
+          new $item($this->request);
+        }
+      }
+    }
+
     $C = new Cron();
     $Controllers = $C->list();
 
@@ -48,7 +59,21 @@ class CronApp extends App
       if (is_callable($controller)) {
         $controller();
       } else {
-        $this->executeController($controller);
+        $this->executeController([], [], $controller);
+        if (method_exists($controller, "after") && $controller->after) {
+          $controller->after();
+        }
+      }
+    }
+
+    //* 调用生命周期“结束”钩子
+    if ($this->LifeCycle['shutdown']) {
+      foreach ($this->LifeCycle['shutdown'] as $item) {
+        if (is_callable($item)) {
+          $item(null);
+        } else {
+          new $item(null);
+        }
       }
     }
   }
