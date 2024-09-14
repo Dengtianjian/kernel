07ae13ba1ad4604cb6fdee5746db6fe18cc8e519
DengTianjian <mail@isdtj.com>
2024-09-14 22:48:43 +0800

    清除索引路由代码

--- Controller/Main/IndexController.php
+++ Controller/Main/IndexController.php
@@ -2,24 +2,8 @@
 
 namespace kernel\Controller\Main;
 
-use Exception;
-use kernel\Foundation\BaseObject;
-use kernel\Foundation\Cache;
 use kernel\Foundation\Controller\Controller;
-use kernel\Foundation\Data\DataConversion;
-use kernel\Foundation\Data\Serializer;
-use kernel\Foundation\Database\PDO\Model;
-use kernel\Foundation\Exception\Exception as ExceptionException;
 use kernel\Foundation\HTTP\Request;
-use kernel\Foundation\HTTP\Response;
-use kernel\Foundation\HTTP\Response\ResponseView;
-use kernel\Foundation\Log;
-use kernel\Foundation\Output;
-use kernel\Foundation\ReturnResult\ReturnResult;
-use kernel\Foundation\Validate\ValidateArray;
-use kernel\Foundation\Validate\ValidateRules;
-use kernel\Foundation\Validate\Validator;
-use kernel\Platform\DiscuzX\Foundation\Database\DiscuzXModel;
 
 class IndexController extends Controller
 {
@@ -30,7 +14,6 @@ class IndexController extends Controller
   }
   public function data()
   {
-    return DiscuzXModel::singleton("common_member")->getAll();
-    return Import("Configs/ConfigFun", [3, 6]);
+ 
   }
 }
